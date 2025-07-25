3329081fbbb2fa4c51bab52d0a91af566b6a61d1
Kurdina_YuS <kurdina.00@gmail.com>
2025-07-25 14:20:36 +0300

    Добавьте файлы проекта.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocsService.Controllers
+{
+
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult GetHello()
+        {
+            return Ok(new { message = "Привет от бэкенда!" });
+        }
+    }
+
+}
---
+++ Views/app.js
@@ -0,0 +1,15 @@
+﻿document.getElementById("fetchBtn").addEventListener("click", async () => {
+    try {
+        const response = await fetch("http://localhost:5126/api/test/hello");
+
+        if (!response.ok) {
+            throw new Error("Ошибка HTTP: " + response.status);
+        }
+
+        const data = await response.json();
+        document.getElementById("output").textContent = data.message;
+    } catch (error) {
+        console.error("Ошибка:", error);
+        document.getElementById("output").textContent = "Не удалось загрузить данные";
+    }
+});
